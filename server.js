57b940d0fafd06877085d626f7c9c82ebfae5238
Mansi <mansii.kulkarni29@gmail.com>
2019-08-04 21:03:25 -0400

    fixed all heroku db errors

--- server.js
+++ server.js
@@ -2,9 +2,7 @@ var express = require('express')
 var app = express()
 var mongoose = require('mongoose')
 
-
-
-mongoose.connect('mongodb://heroku_674vnd6l:qvie32tkbjru83t21rsoml39ml@ds019638.mlab.com:19638/heroku_674vnd6l/wbdv-su19',
+mongoose.connect('mongodb://heroku_674vnd6l:qvie32tkbjru83t21rsoml39ml@ds019638.mlab.com:19638/heroku_674vnd6l',
  {useNewUrlParser: true});
 
 //mongoose.connect('mongodb://localhost:27017/wbdv-su19', {useNewUrlParser: true});
